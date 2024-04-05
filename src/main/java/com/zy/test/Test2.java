948825a11b7906ef25f6c1bc11f2761983aae7f1
zhangyi <1055198744@qq.com>
2024-04-05 22:00:08 +0800

    ddd

---
+++ src/main/java/com/zy/test/Test2.java
@@ -0,0 +1,10 @@
+package com.zy.test;
+
+public class Test2 {
+    public static void main(String[] args) {
+        System.out.println("123456");
+        System.out.println("654321");
+        System.out.println("t");
+    }
+}
+
