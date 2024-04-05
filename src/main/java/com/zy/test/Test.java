dc292f2fea4ff2991c52ee5c4286d9a4b9f37145
zhangyi <1055198744@qq.com>
2024-04-05 21:19:42 +0800

    ddd

--- src/main/java/com/zy/test/Test.java
+++ src/main/java/com/zy/test/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("123456");
         System.out.println("654321");
-        System.out.println("tt");
+        System.out.println("ttyy");
     }
 }
 
